6913487082873d4c6dff45cc4cbf9012086619c7
Пользователь <777draft777@mail.ru>
2026-05-24 21:37:41 +0300

    исправлен вывод в консоле в Задании 1

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
         long d = 100254762L;
         float e = 2.75f;
         double f = 5.123456789d;
-        System.out.println("Значение a: " + a);
-        System.out.println("Значение b: " + b);
-        System.out.println("Значение c: " + c);
-        System.out.println("Значение d: " + d);
-        System.out.println("Значение e: " + e);
-        System.out.println("Значение f: " + f);
+        System.out.println("a " + "= " + a);
+        System.out.println("b " + "= " + b);
+        System.out.println("c " + "= " + c);
+        System.out.println("d " + "= " + d);
+        System.out.println("e " + "= " + e);
+        System.out.println("f " + "= " + f);
 
 
         System.out.println("Задание 2");
