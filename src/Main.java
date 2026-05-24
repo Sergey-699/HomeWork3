8f34066610c9c9f6bc7e960be5ca3dd60cb43bf7
Пользователь <777draft777@mail.ru>
2026-05-24 13:49:45 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание 1");
+        byte a = 2;
+        short b = 25700;
+        int c = -10523625;
+        long d = 100254762L;
+        float e = 2.75f;
+        double f = 5.123456789d;
+        System.out.println("Значение a: " + a);
+        System.out.println("Значение b: " + b);
+        System.out.println("Значение c: " + c);
+        System.out.println("Значение d: " + d);
+        System.out.println("Значение e: " + e);
+        System.out.println("Значение f: " + f);
+
 
     }
-}
 }
